80e0da5d0062a8fbc941aa5a94c543dfcd7b6241
verschollen <Amirarshia.darabi@gmail.com>
2025-06-01 18:26:53 +0200

    update source code file

--- src/main.py
+++ src/main.py
@@ -1,69 +1,6 @@
 import random
 import string
 from abc import ABC, abstractmethod
-
-import nltk
-
-nltk.download('words')
-
-
-class PasswordGenerator(ABC):
-    @abstractmethod
-    def generate(self):
-        pass
-
-
-class PinGenerator(PasswordGenerator):
-    def __init__(self, length):
-        self.length = length
-
-    def generate(self):
-        return ''.join([random.choice(string.digits) for _ in range(self.length)])
-
-
-class RandomPasswordGenerator(PasswordGenerator):
-    def __init__(self, length: int = 8, include_numbers: bool = False, include_symbols: bool = False):
-        self.length = length
-        self.characters = string.ascii_letters
-        if include_numbers:
-            self.characters += string.digits
-
-        if include_symbols:
-            self.characters += string.punctuation
-
-    def generate(self):
-              return ''.join([random.choice(self.characters) for _ in range(self.length)])
-
-
-
-class MemorablePasswordGenerator(PasswordGenerator):
-    def __init__(
-        self,
-        num_of_words: int = 4,
-        separator: str = '-' ,
-        capitalization: bool = False,
-        vocabulary: list = None
-    ):
-
-        if vocabulary is None:
-            vocabulary = nltk.corpus.words.words()
-
-        self.vocabulary = vocabulary
-        self.num_of_words = num_of_words
-        self.capitalization = capitalization
-        self.separator = separator
-
-    def generate(self):
-        password_words = [random.choice(self.vocabulary) for _ in range(self.num_of_words)]
-        if self.capitalization:
-            password_words = [word.upper() if random.choice([True, False]) else word.lower() for word in password_words]
-
-        return self.separator.join(password_words)
-
-
-import random
-import string
-from abc import ABC, abstractmethod
 import nltk
 
 nltk.download('words', quiet=True)
